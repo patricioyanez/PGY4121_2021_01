990eaa4dcd3ebf3e92e0d84ca287223bda80910a
patricioyanez <66755362+patricioyanez@users.noreply.github.com>
2021-09-04 21:07:24 -0400

    ppt

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -24,7 +24,7 @@ const routes: Routes = [
     loadChildren: () => import('./promedio/promedio.module').then( m => m.PromedioPageModule)
   },
   {
-    path: 'detalle',
+    path: ':id',
     loadChildren: () => import('./detalle/detalle.module').then( m => m.DetallePageModule)
   },
 ];
